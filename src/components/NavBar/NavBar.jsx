046caffd18956b704e46104c2baf4269fbac3b28
Thomas Daley <prayerproduction@gmail.com>
2022-05-23 20:03:49 +0100

    layout for nav done

---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,22 @@
+import "./NavBar.scss";
+import Logo from "../../assets/images/Logo/BrainFlix-logo.svg";
+
+export const NavBar = () => {
+  return (
+    <nav className="nav">
+      <img className="nav__image" src={Logo} alt="" />
+      <div className="nav__container">
+        <input
+          className="nav__container-input"
+          type="text"
+          placeholder="Search"
+        />
+
+        <div className="nav__secondary-image"></div>
+      </div>
+      <button className="nav__button" type="submit">
+        UPLOAD
+      </button>
+    </nav>
+  );
+};
