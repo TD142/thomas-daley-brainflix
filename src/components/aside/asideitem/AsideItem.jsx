3f4dfc7612852d614b6ed7af1648c2e0502b2388
Thomas Daley <prayerproduction@gmail.com>
2022-05-27 19:33:27 +0100

    fixed styling for images

--- src/components/aside/asideitem/AsideItem.jsx
+++ src/components/aside/asideitem/AsideItem.jsx
@@ -4,7 +4,9 @@ export const AsideItem = (props) => {
   return (
     <article>
       <div onClick={props.clickFunction} className="aside__container">
-        <img className="aside__image" src={props.image} alt="" />
+        <div>
+          <img className="aside__image" src={props.image} alt="" />
+        </div>
         <div className="aside__inner-container">
           <p className="aside__text">{props.title}</p>
           <p className="aside__secondary-text">{props.channel}</p>
