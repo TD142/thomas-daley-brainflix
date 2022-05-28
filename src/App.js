7e9112ddcfd290690cd1ce4f568d44ce4fbdfa25
Thomas Daley <prayerproduction@gmail.com>
2022-05-28 21:51:38 +0100

    made syntax clearer

--- src/App.js
+++ src/App.js
@@ -8,22 +8,21 @@ import React from "react";
 // on page load the first entry in the videos array is removed so it doesn't appear in the aside.
 const videosOnLoad = videos.slice(1, 9);
 
-console.log(videoOnLoad[0].image);
 class App extends React.Component {
   state = {
     video: videoOnLoad[0],
     videosGroup: videosOnLoad,
   };
 
-  // ** on click event - filters through videos array returns video if it matches the id of the clicked video, then removes video from videos group array by filtering through and only returns videos if value !== selected element id. State is then updated with filtered objects.
+  // ** on click event - filters through video array, returns video if it matches the id of the clicked video, then removes video from videos group array by filtering through and only returns videos if value !== selected element id. State is then updated with filtered objects.
 
   changeVideo = (id) => {
     const selectedVideo = videoOnLoad.filter((videoOnLoad) => {
       return videoOnLoad.id === id;
     });
 
-    const removedVideo = videos.filter((video) => {
-      return video.id !== id;
+    const removedVideo = videosOnLoad.filter((videos) => {
+      return videos.id !== id;
     });
 
     this.setState({ video: selectedVideo[0], videosGroup: removedVideo });
