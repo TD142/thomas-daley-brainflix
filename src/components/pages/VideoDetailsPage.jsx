e316e7110d0097a6806d4229ae51135c0338d6bd
Thomas Daley <prayerproduction@gmail.com>
2022-05-30 15:56:47 +0100

    set up pages

---
+++ src/components/pages/VideoDetailsPage.jsx
@@ -0,0 +1,46 @@
+import { Header } from "../header/Header";
+import videoOnLoad from "../../data/video-details.json";
+import videos from "../../data/videos.json";
+import { Main } from "../main/Main";
+
+import React from "react";
+
+// on page load the first entry in the videos array is removed so it doesn't appear in the aside.
+const videosOnLoad = videos.slice(1, 9);
+
+class VideoDetailsPage extends React.Component {
+  state = {
+    video: videoOnLoad[0],
+    videosGroup: videosOnLoad,
+  };
+
+  // ** on click event - filters through video array, returns video if it matches the id of the clicked video, then removes video from videos group array by filtering through and only returns videos if value !== selected element id. State is then updated with filtered objects.
+
+  changeVideo = (id) => {
+    const selectedVideo = videoOnLoad.filter((videos) => {
+      return videos.id === id;
+    });
+
+    const removedVideo = videos.filter((videos) => {
+      return videos.id !== id;
+    });
+
+    this.setState({ video: selectedVideo[0], videosGroup: removedVideo });
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <Header />
+
+        <Main
+          changeVideo={this.changeVideo}
+          videos={this.state.video}
+          videosGroup={this.state.videosGroup}
+        />
+      </div>
+    );
+  }
+}
+
+export default VideoDetailsPage;
