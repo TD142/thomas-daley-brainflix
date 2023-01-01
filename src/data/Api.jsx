975f4bef40eeb8a8fbaccb599e333a69bb65e7b5
Thomas Daley <prayerproduction@gmail.com>
2023-01-01 21:10:50 +0000

    added env config

--- src/data/Api.jsx
+++ src/data/Api.jsx
@@ -1,3 +1,3 @@
-export const API_URL = "http://localhost:8080/videos/";
+export const API_URL = process.env.REACT_APP_API_URI;
 export const API_KEY = "?api_key=1cf3ad1a-be72-4318-9b75-be3e72728434";
 export const defaultVideoId = "84e96018-4022-434e-80bf-000ce4cd12b8";
